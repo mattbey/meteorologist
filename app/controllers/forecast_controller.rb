35c2def817b304e24bb7f29cc92a235b08ded7db
mattbey <matt.bey@gmail.com>
2017-04-17 19:24:44 -0500

    4/17 Final Code

--- app/controllers/forecast_controller.rb
+++ app/controllers/forecast_controller.rb
@@ -6,9 +6,9 @@ class ForecastController < ApplicationController
     render("forecast/coords_to_weather_form.html.erb")
   end
 
-  def coords_to_weather
-    @lat = params[:user_latitude]
-    @lng = params[:user_longitude]
+    def coords_to_weather
+      @lat = params[:user_latitude]
+      @lng = params[:user_longitude]
 
     # ==========================================================================
     # Your code goes below.
@@ -16,14 +16,14 @@ class ForecastController < ApplicationController
     # The longitude the user input is in the string @lng.
     # ==========================================================================
 
-    url = "https://api.darksky.net/forecast/d19ef5664f3c56194d73d454f4afb89b=#{@lat},#{@lng}"
+    url = "https://api.darksky.net/forecast/d19ef5664f3c56194d73d454f4afb89b/#{@lat},#{@lng}"
 
     parsed_data = JSON.parse(open(url).read)
-    current_temperature = parsed_data["results"][0]["currently"]["temperature"]
-    current_summary = parsed_data["results"][0]["currently"]["summary"]
-    summary_of_next_sixty_minutes = parsed_data["results"][0]["minutely"]["summary"]
-    summary_of_next_several_hours = parsed_data["results"][0]["hourly"]["summary"]
-    summary_of_next_several_days = parsed_data["results"][0]["daily"]["summary"]
+    current_temperature = parsed_data["currently"]["temperature"]
+    current_summary = parsed_data["currently"]["summary"]
+    summary_of_next_sixty_minutes = parsed_data["minutely"]["summary"]
+    summary_of_next_several_hours = parsed_data["hourly"]["summary"]
+    summary_of_next_several_days = parsed_data["daily"]["summary"]
 
 
     @current_temperature = current_temperature
